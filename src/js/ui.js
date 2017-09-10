6d76d1b3e6a633f3cc0233ba421fe19c28bac743
StephyStuff <stephanie.birch13@gmail.com>
2017-09-10 11:23:17 -0400

    Added docblock for show and hid nav function. Audio file now plays and paused on button click.

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,10 +1,31 @@
 (function() {
   'use strict';
+  /**
+   * Shows and hides main nav on hover
+   */
+  $(".main-nav").hover(function(){
+    $(".hidden-nav").show();
+    }, function(){
+      $(".hidden-nav").hide();
+  });
+  /**
+   * Plays and pauses audio on button click
+   * @type {audio}
+   */
+  var audio = $("#beatles")[0];
+  var play = $(".fa-play-circle");
+  var pause = $(".fa-pause-circle");
 
-$(".main-nav").hover(function(){
-  $(".hidden-nav").show();
-  }, function(){
-    $(".hidden-nav").hide();
-});
+  play.click(function(){
+  play.hide();
+  pause.show();
+  audio.play();
+  });
+
+  pause.click(function(){
+    pause.hide();
+    play.show();
+    audio.pause();
+  });
 
 }());
